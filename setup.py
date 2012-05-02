6ba78ce0ad5f398a2b9435ea8514ac111c5df360
Kevin Lyda <kevin@ie.suberic.net>
2012-05-02 18:39:12 +0100

    Packaging cleanups.
    setup.py changes:
      Add test and dist_clean commands.
      Add MANIFEST.in file to make sdist command work.
    Test changes:
      Remove unneeded runall.py script.  Also, __init__.py.
      Mentioned tests and how to run them in README.

--- setup.py
+++ setup.py
@@ -18,9 +18,109 @@
 This installs the chkcrontab command and the crontab.check module.
 """
 
+import os
+import sys
+from distutils import log
 from distutils.core import setup
+from distutils.core import Command
+if sys.version_info < (2, 7):
+  import unittest2 as unittest
+else:
+  import unittest
+
+BASE_DIR = os.path.dirname(globals().get('__file__', os.getcwd()))
+
+
+class TestCommand(Command):
+  description = 'Runs all available tests.'
+  user_options = [ ]
+
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    pass
+
+  def run(self):
+    test_dir = os.path.join(BASE_DIR, 'tests')
+
+    tests = unittest.TestLoader().discover(test_dir)
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(tests)
+    if not result.wasSuccessful():
+      sys.exit(1)
+
+class CleanCommand(Command):
+  description = 'Remove all generated files.'
+  user_options = [ ]
+
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    pass
+
+  def run(self):
+    # Configure for this project.
+    suffixes2del = [ 'MANIFEST', '.pyc' ]
+    dirs2del = [ './build', './dist' ]
+    dirs2ign = [ './.git' ]
+    # End config.
+    doomed = [ ]
+    # Change to base dir.
+    os.chdir(BASE_DIR)
+    for root, dirs, files in os.walk('.'):
+      # Handle root dirs.
+      if root in dirs2ign:
+        continue
+      if root in dirs2del:
+        doomed.append(root)
+      # Handle files.
+      for f in files:
+        accused = os.path.join(root, f)
+        for suffix in suffixes2del:
+          if f.endswith(suffix):
+            doomed.append(accused)
+            break
+        if accused not in doomed:
+          for d2del in dirs2del:
+            if accused.startswith(d2del):
+              doomed.append(accused)
+              break
+      # Handle dirs.
+      for d in dirs:
+        accused = os.path.join(root, d)
+        for d2ign in dirs2ign:
+          if accused.startswith(d2ign):
+            dirs.remove(d)
+            break
+        if d in dirs:
+          for d2del in dirs2del:
+            if accused.startswith(d2del):
+              doomed.append(accused)
+              break
+    # Probably not required, but just to be safe.
+    for accused in doomed:
+      for d2ign in dirs2ign:
+        if accused.startswith(d2ign):
+          doomed.remove(accused)
+          break
+    doomed.sort(reverse=True)
+    for accused in doomed:
+      log.info('removing "%s"', os.path.normpath(accused))
+      if not self.dry_run:
+        try:
+          os.unlink(accused)
+        except:
+          try:
+            os.rmdir(accused)
+          except:
+            log.warn('unable to remove "%s"', os.path.normpath(accused))
 
 setup(
+    cmdclass={'test': TestCommand,
+              'dist_clean': CleanCommand
+             },
     name='chkcrontab',
     version='1.0',
     url='http://code.google.com/p/chkcrontab',
